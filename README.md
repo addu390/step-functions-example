634f9601b7b813840c771164c4bd6ad5fec0ad79
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-04 20:03:41 -0300

    Initial commit for lambda handler and step functions template

---
+++ README.md
@@ -0,0 +1,1 @@
+# Step functions, Lambda and SQS
---
+++ input.py
@@ -0,0 +1,19 @@
+def select_tier(email, message):
+    tier = "UNKNOWN"
+    message = message.lower()
+    if any(word in message for word in ["account", "password"]):
+        tier = "TIER_1"
+    if any(word in message for word in ["computer", "laptop", "printer"]):
+        tier = "TIER_2"
+    if email.strip() == "rhawkey@dal.ca":
+        tier = "TIER_3"
+
+    return tier
+
+
+def form_input(event, context):
+    tier = select_tier(event["email"], event["message"])
+    return {
+        "tier": tier
+    }
+
